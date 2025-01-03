ca9561a59dd0debc0cf628ed3435fc2a9848ec66
darmond157 <bardiarezaeikalantari@gmail.com>
2025-01-03 13:04:19 +0330

    Feat: init codes and manifests

---
+++ Codes/jwt-nodejs-vuejs/Backend/main.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const fastify = require("fastify")({ logger: true });
+
+require("./src/plugins/postgres")(fastify)
+	.then(() => {
+		require("./src/plugins/jwt")(fastify);
+	})
+	.then(() => {
+		require("./src/plugins/bcrypt")(fastify);
+	})
+	.then(() => {
+		require("./src/hooks/jwtAuth")(fastify);
+		require("./src/routes")(fastify);
+		require("./src/plugins/cors")(fastify);
+	});
+
+fastify.listen({ port: process.env.PORT, host: process.env.HOST });
